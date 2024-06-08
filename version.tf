96d980ce9b1754c3c6ee81f2c766796881739ca5
sharonnjoka <sharongandi@gmail.com>
2024-06-08 12:22:17 -0500

    new version

--- version.tf
+++ version.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "4.61.0"
+        version = "4.60.0"
 
     }
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
   
 }
